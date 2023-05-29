21a0c871c3e36ece33ad099fddcc6ea38b7d2ae6
sh1ngekyo <repllimate@gmail.com>
2023-05-29 13:04:49 +0300

    Add enums for weather forecast

---
+++ WeatherForecast.Client.Core.Domain/Models/Enums/WeatherCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecast.Client.Core.Domain.Models.Enums
+{
+    internal class WeatherCode
+    {
+    }
+}
