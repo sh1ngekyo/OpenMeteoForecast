e9a52dc147d92a25c550d926538356be10c55977
sh1ngekyo <repllimate@gmail.com>
2023-05-29 12:44:26 +0300

    Implement user api & setup DI

---
+++ WeatherForecast.Backend.Presentation.UserApi/Controllers/BaseController.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherForecast.Backend.Presentation.UserApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public abstract class BaseController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator =>
+            _mediator ??= HttpContext.RequestServices.GetService<IMediator>()!;
+    }
+}
---
+++ WeatherForecast.Backend.Presentation.UserApi/Models/UpdateUserDto.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+
+using System.ComponentModel.DataAnnotations;
+
+using WeatherForecast.Backend.Core.Application.Common.Mappings;
+using WeatherForecast.Backend.Core.Application.Users.Commands.UpdateUser;
+using WeatherForecast.Backend.Core.Domain;
+
+namespace WeatherForecast.Backend.Presentation.UserApi.Models
+{
+    public class UpdateUserDto : IMapWith<UpdateUserCommand>
+    {
+        [Required]
+        public long? Id { get; set; }
+        public double? Latitude { get; set; }
+        public double? Longitude { get; set; }
+        public Time? Alarm { get; set; }
+        public Time? WarningsStartTime { get; set; }
+        public Time? WarningsEndTime { get; set; }
+
+        public void Mapping(Profile profile)
+        {
+            profile.CreateMap<UpdateUserDto, UpdateUserCommand>()
+                .ForMember(userCommand => userCommand.Id,
+                    opt => opt.MapFrom(userDto => userDto.Id))
+                .ForMember(userCommand => userCommand.Longitude,
+                    opt => opt.MapFrom(userDto => userDto.Longitude))
+                .ForMember(userCommand => userCommand.Latitude,
+                    opt => opt.MapFrom(userDto => userDto.Latitude))
+                .ForMember(userCommand => userCommand.Alarm,
+                    opt => opt.MapFrom(userDto => userDto.Alarm))
+                .ForMember(userCommand => userCommand.WarningsStartTime,
+                    opt => opt.MapFrom(userDto => userDto.WarningsStartTime))
+                .ForMember(userCommand => userCommand.WarningsEndTime,
+                    opt => opt.MapFrom(userDto => userDto.WarningsEndTime));
+        }
+    }
+}
