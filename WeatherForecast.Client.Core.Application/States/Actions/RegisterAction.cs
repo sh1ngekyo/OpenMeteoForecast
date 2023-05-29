5caf512ae72ab2e262ae8c4d4c33ee29c9bbfc8a
sh1ngekyo <repllimate@gmail.com>
2023-05-29 14:12:54 +0300

    Create register action

---
+++ WeatherForecast.Client.Core.Application/States/Actions/RegisterAction.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Telegram.Bot.Types;
+
+using UserRemoteApi.Interfaces;
+
+using WeatherForecast.Client.Core.Domain.Models;
+
+namespace WeatherForecast.Client.Core.Application.States.Actions
+{
+    public class RegisterAction
+    {
+        private readonly IUserManageService _userManageService;
+
+        public RegisterAction(IUserManageService userManageService)
+        {
+            _userManageService = userManageService;
+        }
+
+        public async Task<Response<OutputMessage>> Do(Message message, CancellationToken cancellationToken = default)
+        {
+            var response = await _userManageService.HandleAsync(
+                new RegisterUserCommand
+                {
+                    Id = message.Chat.Id,
+                    Longitude = message.Location.Longitude,
+                    Latitude = message.Location.Latitude,
+                }, cancellationToken);
+            return new Response<OutputMessage>()
+            {
+                Result = new OutputMessage()
+                {
+                    Response = MessageType.RegisterOk
+                },
+                Error = response.Error
+            };
+        }
+    }
+}
