689459f382a8fd0e4ffd93a4263c5c8495871c03
sh1ngekyo <repllimate@gmail.com>
2023-05-29 13:39:58 +0300

    Create UserRemoteApi project

---
+++ UserRemoteApi/Interfaces/IUserManageService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WeatherForecast.Client.Core.Domain.Models;
+
+namespace UserRemoteApi.Interfaces
+{
+    public interface IUserManageService
+    {
+        Task<Response<TResponse>> HandleAsync<TResponse>(IRequest<TResponse> request, CancellationToken cancellationToken = default);
+    }
+}
