08b3b2cb77f0fc9137160820eb6368e7be607bc2
sh1ngekyo <repllimate@gmail.com>
2023-05-29 12:09:33 +0300

    Implement queries in backend.application

---
+++ WeatherForecast.Backend.Core.Application/Users/Queries/GetUser/GetUserQueryHandler.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using MediatR;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherForecast.Backend.Core.Application.Common.Exceptions;
+using WeatherForecast.Backend.Core.Application.Interfaces;
+
+using WeatherForecast.Backend.Core.Domain;
+
+namespace WeatherForecast.Backend.Core.Application.Users.Queries.GetUser
+{
+    public class GetUserQueryHandler : IRequestHandler<GetUserQuery, UserViewModel>
+    {
+        private readonly IUserDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetUserQueryHandler(IUserDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public async Task<UserViewModel> Handle(GetUserQuery request, CancellationToken cancellationToken)
+        {
+            var entity = await _context.Users.FirstOrDefaultAsync(user => user.Id == request.Id, cancellationToken);
+
+            if (entity == null)
+            {
+                throw new NotFoundException(nameof(User), request.Id);
+            }
+
+            return _mapper.Map<UserViewModel>(entity);
+        }
+    }
+}
