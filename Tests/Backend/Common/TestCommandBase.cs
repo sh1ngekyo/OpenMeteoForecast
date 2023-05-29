d97d1fbf3f59649bf107a7ad44107b39f72c42e8
sh1ngekyo <repllimate@gmail.com>
2023-05-29 12:53:11 +0300

    Create tests for backend.core

---
+++ Tests/Backend/Common/TestCommandBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WeatherForecast.Infrastructure.Persistence;
+
+namespace WeatherForecast.Backend.Tests.Common
+{
+    public abstract class TestCommandBase : IDisposable
+    {
+        protected readonly UserDbContext Context;
+
+        public TestCommandBase()
+        {
+            Context = UserContextFactory.Create();
+        }
+
+        public void Dispose()
+        {
+            UserContextFactory.Destroy(Context);
+        }
+    }
+}
