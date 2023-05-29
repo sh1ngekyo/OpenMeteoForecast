99c027809127c2c14bbc4447c6d4c5c28531cc98
sh1ngekyo <repllimate@gmail.com>
2023-05-29 14:14:46 +0300

    Add state strategy

---
+++ WeatherForecast.Client.Core.Application/States/StateStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecast.Client.Core.Application.States
+{
+    internal class StateStrategy
+    {
+    }
+}
