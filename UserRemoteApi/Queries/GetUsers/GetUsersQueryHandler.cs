8662748a88a2a18e4cf2df0f61830ed0332c379f
sh1ngekyo <repllimate@gmail.com>
2023-05-29 14:24:11 +0300

    Create waiting for location state & update UserRemoteApi queries

--- UserRemoteApi/Queries/GetUsers/GetUsersQueryHandler.cs
+++ UserRemoteApi/Queries/GetUsers/GetUsersQueryHandler.cs
@@ -1,12 +1,33 @@
-﻿using System;
+﻿using HttpService;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using UserRemoteApi.Models;
+
 namespace UserRemoteApi.Queries.GetUsers
 {
-    internal class GetUsersQueryHandler
+    public class GetUsersQueryHandler : IRequestHandler<GetUsersQuery, UsersList>
     {
+        private readonly IHttpService _httpService;
+        private readonly IConfiguration _configuration;
+
+        public GetUsersQueryHandler(IHttpService httpService, IConfiguration configuration)
+        {
+            _httpService = httpService;
+            _configuration = configuration;
+        }
+
+        public async Task<UsersList> Handle(GetUsersQuery request, CancellationToken cancellationToken)
+        {
+            var response = await _httpService.GetAsync($"{_configuration.GetRequiredSection("ApiPaths")["Backend"]}", cancellationToken);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<UsersList>();
+        }
     }
 }
---
+++ WeatherForecast.Client.Core.Application/States/WaitingForLocation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherForecast.Client.Core.Application.States
+{
+    public class WaitingForLocation : IState
+    {
+        private readonly IUserManageService _userManageService;
+
+        public WaitingForLocation(IUserManageService userManageService)
+        {
+            _userManageService = userManageService;
+        }
+
+        public async Task<Response<OutputMessage>> Handle(IUserContext context, Message message, CancellationToken cancellationToken = default)
+        {
+            context.UpdateState(StateType.WaitingForNewMessages);
+            if (message.Location is null)
+            {
+                return await context.Update(message, cancellationToken);
+            }
+            var user = await _userManageService.HandleAsync(
+               new GetUserQuery
+               {
+                   Id = message.Chat.Id
+               },
+               cancellationToken);
+            if (user.HasError)
+            {
+                if (user.Error == ErrorMessageType.NotFound)
+                {
+                    return await new RegisterAction(_userManageService).Do(message, cancellationToken);
+                }
+                return new Response<OutputMessage>()
+                {
+                    Error = user.Error,
+                };
+            }
+            return await new UpdateAction(_userManageService).Do(message, cancellationToken);
+        }
+    }
+}
