1e172e0e30163ef2e2eadea656fb6c530bc75c95
sh1ngekyo <repllimate@gmail.com>
2023-05-29 13:55:29 +0300

    Create OpenMeteoRemoteApi

---
+++ OpenMeteoRemoteApi/Interfaces/IWeatherApiWrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WeatherForecast.Client.Core.Domain.Models;
+
+namespace OpenMeteoRemoteApi.Interfaces
+{
+    public interface IWeatherApiWrapper
+    {
+        Task<Response<WeatherResponse?>> GetWeatherAsync(WeatherRequest request, CancellationToken cancellationToken = default);
+    }
+}
