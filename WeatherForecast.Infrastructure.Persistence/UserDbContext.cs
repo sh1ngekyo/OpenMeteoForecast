e2de065307acac0fed045e426340f546c2b74000
sh1ngekyo <repllimate@gmail.com>
2023-05-29 12:19:45 +0300

    Add backend.infrastructure

---
+++ WeatherForecast.Infrastructure.Persistence/UserDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using WeatherForecast.Backend.Core.Application.Interfaces;
+using WeatherForecast.Backend.Core.Domain;
+using WeatherForecast.Infrastructure.Persistence.EntityTypeConfiguration;
+
+namespace WeatherForecast.Infrastructure.Persistence
+{
+    public class UserDbContext : DbContext, IUserDbContext
+    {
+        public UserDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
