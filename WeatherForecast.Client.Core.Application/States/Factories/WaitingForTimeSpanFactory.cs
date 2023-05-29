33f871fd8afe018a167fec534fa8bdc278b50da7
sh1ngekyo <repllimate@gmail.com>
2023-05-29 14:49:26 +0300

    Add state for waiting time from user

---
+++ WeatherForecast.Client.Core.Application/States/Factories/WaitingForTimeSpanFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UserRemoteApi.Interfaces;
+using WeatherForecast.Client.Core.Application.Interfaces;
+using WeatherForecast.Client.Core.Domain.Models.Enums;
+
+namespace WeatherForecast.Client.Core.Application.States.Factories
+{
+    public class WaitingForTimeSpanFactory : IStateFactory
+    {
+        private readonly IUserManageService _userManageService;
+
+        public WaitingForTimeSpanFactory(IUserManageService userManageService)
+        {
+            _userManageService = userManageService;
+        }
+
+        public bool AppliesTo(StateType stateType)
+        {
+            return Enum.GetName(stateType).Equals(nameof(WaitingForTimeSpan));
+        }
+
+        public IState CreateState(StateType stateType)
+        {
+            return new WaitingForTimeSpan(_userManageService);
+        }
+    }
+}
